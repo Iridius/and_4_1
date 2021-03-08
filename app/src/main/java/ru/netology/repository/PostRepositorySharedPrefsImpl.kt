9671f8c0381ceb118178671f26f4c681a90bbe0f
Melkov Denis <d.a.melkov@gmail.com>
2021-03-08 23:14:06 +0300

    add SharedPrefsImpl logic

--- app/src/main/java/ru/netology/repository/PostRepositorySharedPrefsImpl.kt
+++ app/src/main/java/ru/netology/repository/PostRepositorySharedPrefsImpl.kt
@@ -1,30 +1,99 @@
 package ru.netology.repository
 
+import android.content.Context
 import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
+import com.google.gson.Gson
+import com.google.gson.reflect.TypeToken
 import ru.netology.dto.Post
 
-class PostRepositorySharedPrefsImpl: PostRepository {
-    override fun getAll(): LiveData<List<Post>> {
-        TODO("Not yet implemented")
+class PostRepositorySharedPrefsImpl(
+    context: Context
+): PostRepository {
+    private val gson = Gson()
+    private val prefs = context.getSharedPreferences("repository", Context.MODE_PRIVATE)
+    private val type = TypeToken.getParameterized(List::class.java, Post::class.java).type
+    private val key = "posts"
+
+    private var id = 0L
+    private var posts = emptyList<Post>()
+    private val data = MutableLiveData(posts)
+
+    init {
+        prefs.getString(key, null)?.let {
+            posts = gson.fromJson(it, type)
+            data.value = posts
+        }
     }
 
+    override fun getAll(): LiveData<List<Post>> = data
+
     override fun likeById(id: Long) {
-        TODO("Not yet implemented")
+        posts = posts.map {
+            if(it.id != id) it
+            else it.copy(
+                hasAutoLike = !it.hasAutoLike,
+                likes = it.likes + 1 * (if (it.hasAutoLike) -1 else 1)
+            )
+        }
+
+        data.value = posts
+        sync()
     }
 
     override fun shareById(id: Long) {
-        TODO("Not yet implemented")
+        posts = posts.map {
+            if(it.id != id) it
+            else it.copy(shares = it.shares + 1)
+        }
+        data.value = posts
+        sync()
     }
 
     override fun viewById(id: Long) {
-        TODO("Not yet implemented")
+        posts = posts.map {
+            if(it.id != id) it else it.copy(views = it.views + 1)
+        }
+
+        data.value = posts
+        sync()
     }
 
     override fun removeById(id: Long) {
-        TODO("Not yet implemented")
+        posts = posts.filter {
+            it.id != id
+        }
+
+        data.value = posts
+        sync()
     }
 
     override fun save(post: Post) {
-        TODO("Not yet implemented")
+        if(post.id == 0L) {
+            posts = listOf(
+                post.copy(
+                    id = id++,
+                    title = "post title",
+                    subTitle = "subtitle"
+                )
+            ) + posts
+
+            data.value = posts
+            return
+        }
+
+        posts = posts.map {
+            if(it.id != post.id) it else it.copy(content = post.content)
+        }
+
+        data.value = posts
+        sync()
+    }
+
+    private fun sync() {
+        with(prefs.edit()) {
+            putString(key, gson.toJson(posts))
+            apply()
+        }
     }
 }
